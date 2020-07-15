ec562b8be822f027cfd8f96528f75fca90ea7762
Himank289 <68347821+Himank289@users.noreply.github.com>
2020-07-15 11:40:10 -0700

    Add files via upload

---
+++ python lab 11 july.py
@@ -0,0 +1,186 @@
+print("hello how are you all")
+#python program to demonstrate numeric value
+a=5
+print("type of a ",type(a))
+b=5.0
+print("\ntype of b:",type(b))
+c=2+4
+print("\n type of c",type(c))
+#creating a list
+list=[]
+print("initial empty list")
+print(list)
+list1=["welcome to programming"]
+print("\n list with the use of string")
+print(list1)
+list2=["hello",1,"himank","tyagi"]
+print("\n list containing multiple values")
+print(list2[0])
+print(list2[3])
+list3=[[1,2]],["data structure"]
+print("\n multi dimensional list")
+print(list3)
+a=[1,2,"hello","tyagi",7]
+print("accessing using positive indices")
+print(a[0])
+print(a[2])
+print("accessing using negative indices ")
+print(a[-2])
+print(a[-1])
+b=[1,"r","p",4,"s"]
+b.remove("p")
+print(b)
+print(b.pop(3))
+print(b.pop())
+print(b)
+c=["hello","tyagi",2,5,"himank"]
+print(c.index(2))
+print(c.count(2))
+c.reverse()
+print(c)
+d=[23,45,65,46,34,32]
+print("initial list")
+print(d)
+d.remove(23)
+print(d)
+print(d.pop(3))
+print(d.pop())
+e=[]
+e.append(1)
+e.append(3)
+e.append("hello")
+e.append("himank")
+e.append("tyagi")
+print("after appending the list")
+print(e)
+e.insert(1,"hi")
+e.insert(4,"hlw")
+print(e)
+e.extend([7,"python","programming","is","best"])
+print(e)
+print("line a\n lineb\n line c")
+print("linea\t line b\t line c")
+print("line a\\tline b\\tline c")
+print("linea\\nlineb\\nlinec")
+print(r"line a\n lineb\n line c")
+print(bool(5))
+print(bool(4.56))
+print(bool("tools sqa"))
+print(bool("tools squery"))
+print(bool(0.0))
+print(bool(""))
+print(bool(''))
+f=11
+print(f)
+f=28.5
+print(f)
+x=3
+y=4
+y=pow(3,4)
+print(y)
+z=-5
+print(z)
+x=5
+y=6
+print("addition",x+y)
+print("subtarction",x-y)
+print("divison",x/y)
+print("multiplication",x*y)
+print("comparison",x>y)
+print("comparison",x<y)
+print("comparison",x>=y)
+print("comparison",x<=y)
+print("comparison",x==y)
+print("comparison",x!=y)
+x=True
+y= False
+print("logical",x and y)
+print("logical",x or y)
+print("logical", not y)
+x1=5
+y1=5
+x2="hello"
+y2="hello"
+print(x2 is y2)
+print(x1 is not y1)
+x="hello world"
+print( "h" in x)
+print("world"  not in   x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
